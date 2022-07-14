bd1b9336305d3364e73484ca436c56a7d60afde6
x0y14 <ya01ma08@gmail.com>
2022-07-14 16:53:45 +0900

    プロセス立ち上げを別のところでするようになったんだと思うので削除

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -21,23 +21,6 @@ func main() {
 	// bubble teaの用意
 	p := tea.NewProgram(tuiClient.InitializeModel(ch, &client))
 
-	// streamを受信するプロセスを立ち上げる
-	//go func() {
-	//	stream, err := client.GreetStream(context.Background(),
-	//		connect.NewRequest(&greetv1.GreetStreamRequest{Name: "1"}))
-	//	if err != nil {
-	//		log.Fatalln(err)
-	//	}
-	//	for stream.Receive() {
-	//		msg := stream.Msg()
-	//		// 受け取ったらbubble teaにcmd（外部操作）として通知してあげる。
-	//		p.Send(tuiClient.ReceiveNotice{
-	//			Id:      msg.Id,
-	//			Content: msg.Text,
-	//		})
-	//	}
-	//}()
-
 	if err := p.Start(); err != nil {
 		log.Fatalln(err)
 	}
